8fa68c69b9f954892b0d2050d6f4725a112b3e0e
Cyprien Le Pannérer <cyplp@free.fr>
2013-04-27 14:15:04 +0200

    initial commit

---
+++ README.txt
@@ -0,0 +1,13 @@
+===================
+alerte.constitution
+===================
+
+.. |date| date::
+
+:Author: Cyprien Le Pannerer <clepannerer@edd.fr>
+:Date: |date|
+
+Assemble edd alert for preparing for sending
+
+.. contents:: Table des matières
+.. sectnum::
---
+++ setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+version = open('version.txt').read().strip()
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(name='cyplp.timelapsetaker',
+      version=version,
+      description="application for timelapse",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='template',
+      author=u'Cyprien Le Pannérer',
+      author_email='clepannerer@edd.fr',
+      url='',
+      packages=find_packages(),
+      namespace_packages=['cyplp'],
+      include_package_data=True,
+      package_data={'cyplp.timelapsetaler': ['README.txt', 'CHANGES.txt', 'MANIFEST.in']},
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+
+          # -*- Extra requirements: -*-
+      ],
+      entry_points={
+	  'console_scripts': [
+	      'timelapse = cyplp.timelapsetaker:main',
+          ],
+      },
+      )
